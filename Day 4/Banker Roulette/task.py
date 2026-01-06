806223c696591325fb8d185f1fa1fb3e4aa9df5f
maruthi <maruthi.wsi@gmail.com>
2026-01-05 20:16:57 -0500

    Day4- Randomization and Lists

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,9 @@
+import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_index = random.randint(0, len(friends) - 1)
+who_pay_the_bill = friends[random_index]
+print(who_pay_the_bill)
+
+choice = random.choice(friends)
+print(choice)
