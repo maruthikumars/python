94d529c2191106595ae9c934c91991d36a477eed
maruthi <maruthi.wsi@gmail.com>
2026-01-10 21:38:14 -0500

    Day10- Functions outputs

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,14 @@
+def my_function():
+    result = 2 * 3
+    return result
+
+
+print(my_function())
+
+
+
+def format_name(first_name, last_name):
+    formatted_name = first_name.title() + " " + last_name.title()
+    return formatted_name
+
+print(format_name("MARUHTI", "kumar"))
